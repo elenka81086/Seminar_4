a5a6d67ecc4917bca6c3786b9a38d78138b4dd9c
Elena <elenka81086@mail.ru>
2022-11-25 17:06:18 +0300

    задача 27

---
+++ problem_27/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход 
+число и выдает сумму цифр в числе */
+
+
+int getSumFigureOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int figure = number % 10;
+        sum += figure;
+        int element = number / 10;
+        number = element;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = getSumFigureOfDigits(number);
+Console.WriteLine($"Для числа {number}, сумма цифр составляет {result}.");
+   
+   
+
+
+
+
+
