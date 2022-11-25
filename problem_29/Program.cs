a36fa5d7749831714cb00010010c37e8600d87af
Elena <elenka81086@mail.ru>
2022-11-25 15:09:23 +0300

    задача 29

---
+++ problem_29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая задаёт массив из 8 
+элементов и выводит их на экран*/
+
+
+int [,] matr = new int [2, 4];
+{
+    Console.Write("[");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 30);
+            Console.Write($" {matr[i, j]} ");
+        }
+    } 
+    Console.Write("]");
+}
