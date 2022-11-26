48a4ad9f95ee9f7248cebc2a29b180ddfb2c7430
Elena <elenka81086@mail.ru>
2022-11-26 09:20:40 +0300

    задача 25

---
+++ problem_25/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите цикл, который принимает на вход два числа (А и В) 
+и возводит число А в натуральную степень В.*/
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int multNumberByNumber(int A, int B)
+{
+    int mult = 1;
+    for (int number = 1; number <= numberB; number++)
+    {
+        mult *= numberA;
+    }
+    return mult;
+}
+
+int result = multNumberByNumber(numberA, numberB);
+Console.WriteLine($"Число {numberA} в натуральной степени числа {numberB} = {result}");
